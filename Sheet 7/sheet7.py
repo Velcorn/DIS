936b2ae61bef9573c5d0d304bcfa2a3ffa44992c
Velcorn <Velcorn@users.noreply.github.com>
2020-07-12 14:28:02 +0200

    Minor text fixes

--- Sheet 7/sheet7.py
+++ Sheet 7/sheet7.py
@@ -46,24 +46,25 @@ while NUMBER != 0:
     else:
         # Combinations from previous itemsets if k > 2.
         candidates = []
+        # Generate possible candidates.
         for i1 in itemsets:
             for i2 in itemsets:
                 if i1[:-1] == i2[:-1] and i1[-1] < i2[-1]:
-                    c = list(i1)
-                    c.append(i2[-1])
-                    # Add to candidates if all subsets are frequent itemsets.
-                    if all(s in itemsets for s in combinations(c, K-1)):
-                        candidates.append(tuple(c))
+                    pc = list(i1)
+                    pc.append(i2[-1])
+                    # Add possible candidate to candidates if all subsets are frequent itemsets.
+                    if all(s in itemsets for s in combinations(pc, K - 1)):
+                        candidates.append(tuple(pc))
 
     # Get counts of k-itemsets.
     itemsets_k = {}
     for t in transactions:
-        for c in candidates:
-            if t.issuperset(c):
-                if c in itemsets_k:
-                    itemsets_k[c] += 1
+        for pc in candidates:
+            if t.issuperset(pc):
+                if pc in itemsets_k:
+                    itemsets_k[pc] += 1
                 else:
-                    itemsets_k[c] = 1
+                    itemsets_k[pc] = 1
 
     # Calculate support and round to 2 decimals.
     for i in itemsets_k:
